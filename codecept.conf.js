ede6688a14fb3d9f03acdb466674b783f0810aa2
ThanNguyen <thannthse183895@fpt.edu.vn>
2024-09-27 19:58:24 +0700

    Update codecept.conf.js

--- codecept.conf.js
+++ codecept.conf.js
@@ -3,13 +3,13 @@ exports.config = {
   helpers: {
     Appium: {
       appiumV2: true,
-      app: "C:\\Users\\THAN\\AndroidStudioProjects\\Calculator\\app\\build\\outputs\\apk\\androidTest\\debug\\app-debug-androidTest.apk",
+      app: "C:\\Users\\THAN\\AndroidStudioProjects\\Calculator\\app\\build\\outputs\\apk\\androidTest\\debug\\app-debug-androidTest.apk",//change the path to your apk file, remember to install the app on your device or emulator first
       platform: 'Android',
       device: 'emulator',
       desiredCapabilities: {
         appPackage: 'com.gfg.calculator',
         appActivity: 'com.gfg.calculator.MainActivity',
-        deviceName: 'emulator-5554', 
+        deviceName: 'emulator-5554',  //change to your device name or emulator name // adb devices for device name on Android Studio
         automationName: 'UiAutomator2', 
       }
     }
